8d7a0ba1f9d54d96052a54db1eab8efb1a43d01a
natan1563 <natromafe@gmail.com>
2023-02-10 19:23:41 -0300

    Refactor route to laravel pattern

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::post('/users', [UserController::class, 'store']);
+Route::resource('/users', UserController::class);
